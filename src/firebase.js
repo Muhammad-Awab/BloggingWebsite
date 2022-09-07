01754b0d8785a2f635a066529d20306f34fd56a4
Muhammad Awab <muhammadawab82002@gmail.com>
2022-09-07 17:12:02 +0500

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "apikey",
+  authDomain: "test",
+  projectId: "test",
+  storageBucket: "test",
+  messagingSenderId: "your id",
+  appId: "appid",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
